a5d9f1c3ae8cac18f4874c41234d15550e342081
eduarger <germaneduardo@MacBook-Pro-de-German.local>
2015-04-11 17:07:03 -0500

    upload of the project

--- plot1.R
+++ plot1.R
@@ -1,7 +1,16 @@
 # plot 1
-# load the filter data frame that was filter with cur_data.R
-load("data_fil.Rda")
-# choosing the device
+#R script to filter the data into the dates required
+# define the name of the data
+zipfile="household_power_consumption.zip" 
+file="household_power_consumption.txt"
+data <- read.csv(unz(zipfile, file), sep=";", stringsAsFactors=FALSE)
+# we will use data from 2007-02-01 to 2007-02-02, we will filter the data
+data$Date <- paste(data$Date, data$Time)
+data$Date=strptime(data$Date, "%d/%m/%Y %H:%M:%S")
+data_fil <- subset(data, Date >= "2007-02-01" & Date <= "2007-02-03")
+numcols <- names(data_fil)[3:length(names(data))]
+data_fil[numcols] <- lapply(data_fil[numcols], as.numeric)
+# choosing the device with the specific features
 png('plot1.png', width=480, height=480, units="px", bg = "transparent")
 #plotting 
 hist(data_fil$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power(Kilowatts)")
